dec45d96b4f6ddce89918af0388d06c608d9b9a8
=Taufiq Hidayah <hidayaht_taufiq@yahoo.com>
2018-04-10 19:27:11 +0700

    Firs Design Activity Main

---
+++ app/src/main/java/com/taufiqhidayah/pemilu/ProfilActiviry.java
@@ -0,0 +1,13 @@
+package com.taufiqhidayah.pemilu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfilActiviry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_activiry);
+    }
+}
