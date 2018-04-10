84c8e159510152c5826b2f0d6f3cb45cc730be3d
=Taufiq Hidayah <hidayaht_taufiq@yahoo.com>
2018-04-10 23:11:04 +0700

    Add Countdown

--- app/src/main/java/com/taufiqhidayah/pemilu/MainActivity.java
+++ app/src/main/java/com/taufiqhidayah/pemilu/MainActivity.java
@@ -1,13 +1,83 @@
 package com.taufiqhidayah.pemilu;
 
+import android.annotation.SuppressLint;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtTimerDay, txtTimerHour, txtTimerMinute, txtTimerSecond;
+    private TextView tvEvent;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+        txtTimerDay = (TextView) findViewById(R.id.txtTimerDay);
+        txtTimerHour = (TextView) findViewById(R.id.txtTimerHour);
+        txtTimerMinute = (TextView) findViewById(R.id.txtTimerMinute);
+        txtTimerSecond = (TextView) findViewById(R.id.txtTimerSecond);
+        tvEvent = (TextView) findViewById(R.id.tvhappyevent);
+
+        countDownStart();
+    }
+
+    private void countDownStart() {
+        handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @SuppressLint({"DefaultLocale", "SetTextI18n"})
+            @Override
+            public void run() {
+                handler.postDelayed(this, 1000);
+                try {
+                    @SuppressLint("SimpleDateFormat") SimpleDateFormat dateFormat = new SimpleDateFormat(
+                            "yyyy-MM-dd");
+                    // Please here set your event date//YYYY-MM-DD
+                    Date futureDate = dateFormat.parse("2018-06-27");
+                    Date currentDate = new Date();
+                    if (!currentDate.after(futureDate)) {
+                        long diff = futureDate.getTime()
+                                - currentDate.getTime();
+                        long days = diff / (24 * 60 * 60 * 1000);
+                        diff -= days * (24 * 60 * 60 * 1000);
+                        long hours = diff / (60 * 60 * 1000);
+                        diff -= hours * (60 * 60 * 1000);
+                        long minutes = diff / (60 * 1000);
+                        diff -= minutes * (60 * 1000);
+                        long seconds = diff / 1000;
+                        txtTimerDay.setText("" + String.format("%02d", days));
+                        txtTimerHour.setText("" + String.format("%02d", hours));
+                        txtTimerMinute.setText(""
+                                + String.format("%02d", minutes));
+                        txtTimerSecond.setText(""
+                                + String.format("%02d", seconds));
+                    } else {
+                        tvEvent.setVisibility(View.VISIBLE);
+                        tvEvent.setText("The event started!");
+                        textViewGone();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1000);
+    }
+
+    private void textViewGone() {
+        findViewById(R.id.LinearLayout10).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout11).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout12).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout13).setVisibility(View.GONE);
+
     }
 }
